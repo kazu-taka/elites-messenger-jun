1edbceee463c66d22d2b2de33ba7dfdeaf5a2aca
SusumuWatanabe <wzensum@gmail.com>
2016-08-02 15:19:39 +0900

    Userモデルにname,thumbnail,agreementを追加

--- elites-messenger-repo/db/migrate/20160802044432_devise_create_users.rb
+++ elites-messenger-repo/db/migrate/20160802044432_devise_create_users.rb
@@ -5,6 +5,11 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      ## nameとthumbnailとagreementを追加
+      t.string :name, null: false
+      t.string :thumbnail
+      t.boolean :agreement, null: false
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
