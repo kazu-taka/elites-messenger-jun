dc3d23e98a38fee28e6ef1420a68e30f91e44bc1
SusumuWatanabe <wzensum@gmail.com>
2016-08-02 15:25:22 +0900

    ホーム画面追加とログインリダイレクト設定

--- elites-messenger-repo/app/controllers/application_controller.rb
+++ elites-messenger-repo/app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # ログインしていない場合はログイン画面にリダイレクトする
+  before_action :authenticate_user!
+
   # deviseのコントローラを実行する時は「configure_permitted_parameters」メソッドを実行する
   before_action :configure_permitted_parameters, if: :devise_controller?
 
