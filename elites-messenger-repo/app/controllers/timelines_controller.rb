7e08af9bc3293c2182e4005789c95380fdc8107d
SusumuWatanabe <wzensum@gmail.com>
2016-08-03 17:34:09 +0900

    メッセージの返信

--- elites-messenger-repo/app/controllers/timelines_controller.rb
+++ elites-messenger-repo/app/controllers/timelines_controller.rb
@@ -3,9 +3,15 @@ class TimelinesController < ApplicationController
     # メッセージ入力
     @input_message = params[:id] ? Timeline.find(params[:id]) : Timeline.new
     # タイムラインを取得
-    @timeline = Timeline.includes(:user).user_filter(params[:filter_user_id]).order('updated_at DESC')
+    @timeline = Timeline.includes(:user).not_reply.user_filter(params[:filter_user_id]).order('updated_at DESC')
     # ユーザ一覧を取得
     @users = User.all
+
+    if params[:reply_id]
+      # 返信時は返信のタイムライン情報を取得
+      @reply_timeline = Timeline.find(params[:reply_id])
+    end
+
   end
 
   def create
@@ -42,7 +48,7 @@ class TimelinesController < ApplicationController
 
   private
   def input_message_param
-    params.require(:timeline).permit(:message)
+    params.require(:timeline).permit(:message, :reply_id)
   end
 
 end
---
+++ elites-messenger-repo/db/migrate/20160803062457_add_parent_id_to_timelines.rb
@@ -0,0 +1,5 @@
+class AddParentIdToTimelines < ActiveRecord::Migration
+  def change
+    add_column :timelines, :reply_id, :integer
+  end
+end
