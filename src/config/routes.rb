888f42caf3b61e386a7b6ca470607e298438daae
kazu-taka <k.takahashi@nowall.co.jp>
2016-10-17 19:13:58 +0900

    letter_opener_webを削除

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -8,10 +8,7 @@ Rails.application.routes.draw do
   end
 
   devise_for :users
-  
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
