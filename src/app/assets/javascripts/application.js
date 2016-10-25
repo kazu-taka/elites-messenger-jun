1f4f279478cae4e91d08f8ff8d3caf8117ed3003
Atsushi Yasuda <atsushi.yasuda.jp@gmail.com>
2016-10-25 13:14:17 +0900

    refactoring coffee

--- src/app/assets/javascripts/application.js
+++ src/app/assets/javascripts/application.js
@@ -15,26 +15,3 @@
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
-$(function() {
-  var initPostButtonEvent;
-  initPostButtonEvent = function() {
-    return $('form.input_message_form input.post').click((function(_this) {
-      return function(e) {
-        var form;
-        form = $('form.input_message_form');
-        form.removeAttr('data-remote');
-        form.removeData("remote");
-        return form.attr('action', form.attr('action').replace('.json', ''));
-      };
-    })(this));
-  };
-  initPostButtonEvent();
-  return $('form.input_message_form').on('ajax:complete', function(event, data, status) {
-    var json;
-    if (status === 'success') {
-      json = JSON.parse(data.responseText);
-      $('div.timeline').prepend($(json.timeline));
-      return initPostButtonEvent();
-    }
-  });
-});
